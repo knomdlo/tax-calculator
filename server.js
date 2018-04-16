84a6db8a7a228d4aadb5991332b87658a3f79d41
Banu Prakash J <bp270292@gmail.com>
2018-04-16 15:35:51 +0530

    To satisfy heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let expressConfig = require('./routes')
 let http = require('http').Server(expressConfig)
 
-let server = http.listen(4000, () => {
-    console.log('server is listening on port', server.address().port)
+let server = http.listen(process.env.PORT || 4000, () => {
+	console.log('server is listening on port', server.address().port)
 })
